da1e8e4c7c1bf63152d8cea016afd4c73cd11f42
oakjedsada <oakjedsada@gmail.com>
2026-06-11 18:01:07 +0700

    side bar
    เพื่ม side bar ข้อมูลเข้าไป

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Navbar } from './navbar/navbar';
 import { Footer } from './footer/footer';
 import { Loan } from './loan/loan';
+import { Sidebar } from './sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Footer, Loan],
+  imports: [Navbar, Footer, Loan, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/sidebar/sidebar.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [MatIconModule],
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.css'
+})
+export class Sidebar {}
